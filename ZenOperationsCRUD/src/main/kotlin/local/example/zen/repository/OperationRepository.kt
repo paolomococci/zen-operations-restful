24ced4f7c16c7a0649eb355bf35c2226c99a65a6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-06-25 16:33:37 +0200

    Create OperationRepository.kt

---
+++ ZenOperationsCRUD/src/main/kotlin/local/example/zen/repository/OperationRepository.kt
@@ -0,0 +1,26 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.zen.repository
+
+import local.example.zen.model.Operation
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+
+@RepositoryRestResource
+interface OperationRepository : PagingAndSortingRepository<Operation, Long>
