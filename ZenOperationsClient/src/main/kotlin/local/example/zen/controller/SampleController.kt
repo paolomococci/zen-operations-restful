d9af6de6439b0152e46583223c6f0255e67dcd49
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-07-01 05:54:50 +0200

    Create SampleController.kt

---
+++ ZenOperationsClient/src/main/kotlin/local/example/zen/controller/SampleController.kt
@@ -0,0 +1,37 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.zen.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@Controller
+class SampleController {
+
+    @GetMapping(value = ["/sample"])
+    fun greeting(@RequestParam(
+            name = "name",
+            required = false,
+            defaultValue = "everyone") name: String, model: Model): String {
+        model.addAttribute("name", name)
+        return "sample"
+    }
+}
