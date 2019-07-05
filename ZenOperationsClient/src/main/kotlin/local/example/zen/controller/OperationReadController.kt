79f189ea16a5680dcb2f06de2c21918c03ed4227
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-07-05 06:47:16 +0200

    Create OperationReadController.kt

---
+++ ZenOperationsClient/src/main/kotlin/local/example/zen/controller/OperationReadController.kt
@@ -0,0 +1,31 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.zen.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class OperationReadController {
+
+    @GetMapping(value = ["/operation-read"])
+    fun operationRead(): String {
+        return "operation-read"
+    }
+}
