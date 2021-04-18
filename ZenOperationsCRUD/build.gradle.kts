ca0ec030381ba97d1aef5a1a26514d0221b1450d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-18 14:23:53 +0200

    Update build.gradle.kts

--- ZenOperationsCRUD/build.gradle.kts
+++ ZenOperationsCRUD/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("plugin.jpa") version "1.3.41"
-    id("org.springframework.boot") version "2.1.6.RELEASE"
-    id("io.spring.dependency-management") version "1.0.7.RELEASE"
-    kotlin("jvm") version "1.3.41"
-    kotlin("plugin.spring") version "1.3.41"
+	id("org.springframework.boot") version "2.4.5"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.32"
+	kotlin("plugin.spring") version "1.4.32"
+	kotlin("plugin.jpa") version "1.4.32"
 }
 
 group = "local.example"
