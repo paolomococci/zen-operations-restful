09a12afad836771a5b89995ce652327044c29880
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-12-22 08:00:24 +0100

    Update build.gradle.kts

--- ZenOperationsClient/build.gradle.kts
+++ ZenOperationsClient/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("com.jayway.jsonpath:json-path:2.6.0")
     implementation("org.webjars:angularjs:2.0.0-alpha.22")
-    implementation("org.webjars:bootstrap:4.3.1")
+    implementation("org.webjars:bootstrap:5.1.3")
     implementation("org.webjars:jquery:3.4.1")
     implementation("org.webjars:popper.js:1.15.0")
     implementation("org.webjars:font-awesome:5.9.0")
