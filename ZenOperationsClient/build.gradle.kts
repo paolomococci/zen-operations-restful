466c79f2332b98aea773cc3e98f0f7e17042d80e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-12-22 07:58:40 +0100

    Update build.gradle.kts

--- ZenOperationsClient/build.gradle.kts
+++ ZenOperationsClient/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.8")
+    implementation("org.projectlombok:lombok:1.18.22")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("com.jayway.jsonpath:json-path:2.4.0")
